930ff01cf8ebe8d377e010d0fbf05d4a2cd8b300
moxianfeng <michael@qq.com>
2023-02-18 00:18:21 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/moxianfeng/redis-transfer
+
+go 1.20
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/spf13/cobra v1.6.1
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.27.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"log"
+
+	"github.com/go-redis/redis"
+	"github.com/spf13/cobra"
+)
+
+type Options struct {
+	SourceAddress       []string
+	SourcePassword      string
+	DestinationPassword string
+	DestinationAddress  string
+	DestinationDB       int
+}
+
+var options Options
+var rootCmd = cobra.Command{
+	RunE: func(cmd *cobra.Command, args []string) error {
+		dstClient := redis.NewClient(&redis.Options{
+			Addr:     options.DestinationAddress,
+			Password: options.DestinationPassword,
+			DB:       options.DestinationDB,
+		})
+
+		srcClient := redis.NewClusterClient(&redis.ClusterOptions{
+			Addrs:    options.SourceAddress,
+			Password: options.SourcePassword,
+		})
+
+		sourceKeys := srcClient.Keys("*")
+		keys, err := sourceKeys.Result()
+		if err != nil {
+			return err
+		}
+
+		log.Printf("Keys got %d keys\n", len(keys))
+
+		successCount := 0
+
+		for _, k := range keys {
+			getResult := srcClient.Get(k)
+			v, err := getResult.Result()
+			if err != nil {
+				return err
+			}
+
+			ttlResult := srcClient.TTL(k)
+			ttl, err := ttlResult.Result()
+			if err != nil {
+				return err
+			}
+
+			ret := dstClient.Set(k, v, ttl)
+			_, err = ret.Result()
+			if err != nil {
+				return err
+			}
+
+			successCount++
+		}
+
+		log.Printf("Successed count %d\n", successCount)
+		return nil
+	},
+}
+
+func main() {
+	err := rootCmd.Execute()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringArrayVarP(&options.SourceAddress, "source-address", "", []string{}, "source addresses")
+	rootCmd.PersistentFlags().StringVarP(&options.SourcePassword, "source-password", "", "", "source password")
+
+	rootCmd.PersistentFlags().StringVarP(&options.DestinationAddress, "dest-address", "", "", "destination addresses")
+	rootCmd.PersistentFlags().StringVarP(&options.DestinationPassword, "dest-password", "", "", "destination password")
+	rootCmd.PersistentFlags().IntVarP(&options.DestinationDB, "dest-db", "", 0, "destination db")
+}
